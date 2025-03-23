165fd33986d87fe380b0a2d2b33506e86953f956
Vedant005 <vedantkanekar909@gmail.com>
2025-03-24 00:22:12 +0530

    feat: introduced single page route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router";
 import GigsPage from "./pages/GigsPage";
 import Home from "./pages/HomePage";
+import SingleGigPage from "./pages/SingleGigPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gigs" element={<GigsPage />} />
+        <Route path="/gigs/:gigId" element={<SingleGigPage />} />
       </Routes>
     </>
   );
