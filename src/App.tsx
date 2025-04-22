632fba99c0ac90698af180779a937b6c08baf249
Vedant005 <vedantkanekar909@gmail.com>
2025-04-22 20:14:44 +0530

    feat : added routes to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router";
-import GigsPage from "./pages/GigsPage";
 import Home from "./pages/HomePage";
+import GigsPage from "./pages/GigsPage";
 import SingleGigPage from "./pages/SingleGigPage";
+import AuthGuard from "./components/AuthGuard";
+import FreelancerLogin from "./components/FreelancerLogin";
+import FreelancerSignup from "./components/FreelancerSingup";
+import ProfilePage from "./pages/ProfilePage";
+import ClientLogin from "./components/clientLogin";
+import ClientSignup from "./components/clientSignup";
+import ClientDashboard from "./pages/clientDashboard";
+import CreateGig from "./components/createGIg";
+import Redirection from "./components/Redirection";
+import ClientProfilePage from "./pages/clientProfilePage,";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/freelancer/login" element={<FreelancerLogin />} />
+        <Route path="/freelancer/signup" element={<FreelancerSignup />} />
+
+        {/* Protected Routes */}
+
         <Route path="/gigs" element={<GigsPage />} />
         <Route path="/gigs/:gigId" element={<SingleGigPage />} />
+        <Route path="/freelancer/profile" element={<ProfilePage />} />
+
+        <Route path="/client/login" element={<ClientLogin />} />
+        <Route path="/client/signup" element={<ClientSignup />} />
+        <Route path="/client/dashboard" element={<ClientDashboard />} />
+        <Route path="/client/create-gig" element={<CreateGig />} />
+        <Route path="/client/profile" element={<ClientProfilePage />} />
+
+        <Route path="/redirection" element={<Redirection />} />
       </Routes>
     </>
   );
